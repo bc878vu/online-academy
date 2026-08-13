2c847680fae982878276ab388a0045f91b4da5cc
Asad Amanat Ali <270855559+bc878vu@users.noreply.github.com>
2026-08-14 04:02:22 +0500

    Apply lightweight performance styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
+import "./performance.css";
 import App from "./App.jsx";
 
 // YouTube's IFrame API is intentionally NOT loaded here.
